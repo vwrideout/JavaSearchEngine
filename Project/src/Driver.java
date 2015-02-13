ef48f12d5dd47364a444430141fce7a99bd8abd5
vwrideout <vwrideout@usfca.edu>
2015-02-12 20:26:25 -0800

    forgot to save driver before last sync

--- Project/src/Driver.java
+++ Project/src/Driver.java
@@ -4,7 +4,10 @@ import java.nio.file.FileSystems;
 public class Driver {
 
 	public static void main(String[] args) throws Exception {
-
+		Configuration config = new Configuration(path);
+		InvertedIndexBuilder builder = new InvertedIndexBuilder(config.getInputPath(), config.useDigitDelimiter());
+		InvertedIndex index = builder.build();
+		tostring index into config.getOutputPath()
 	}
 	
 }
