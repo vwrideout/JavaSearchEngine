a8e4da768b3a7195c029ed3fb466bb9bf6a72026
vwrideout <vwrideout@usfca.edu>
2015-04-26 14:44:12 -0700

    minor touchup, having a conceptual problem

--- Project/src/WebCrawler.java
+++ Project/src/WebCrawler.java
@@ -15,12 +15,10 @@ public class WebCrawler {
 	private int numThreads;
 	private URI seed;
 	private Pattern delimiter;
-	private int pagesCrawled;
 	
 	public WebCrawler(URI seed, boolean digitDelimiter, int numThreads){
 		this.seed = seed;
 		this.numThreads = numThreads;
-		this.pagesCrawled = 0;
 		if(digitDelimiter)
 			this.delimiter = Pattern.compile("[^a-zA-Z]+");
 		else
@@ -32,14 +30,17 @@ public class WebCrawler {
 		queue = new WorkQueue(numThreads);
 		queue.execute(new CrawlWorker(seed));
 		synchronized(visited){
-			try{
-				visited.wait();
-			}catch(InterruptedException ie){
-				System.out.println(ie.getMessage());
+			while(!queue.isEmpty() && visited.size() < 50){
+				try{
+					visited.wait();
+				}catch(InterruptedException ie){
+					System.out.println(ie.getMessage());
+				}
 			}
 		}
 		queue.shutdown();
 		queue.awaitTermination();
+		return index;
 	}
 	
 	private class CrawlWorker implements Runnable{
@@ -53,13 +54,15 @@ public class WebCrawler {
 			String data = HTTPFetcher.download(page.getHost(), page.getPath());
 			ArrayList<String> links = HTMLLinkParser.listLinks(data);
 			for(String str: links){
-				URI link = new URI(str).resolve(page);
-				synchronized(visited){
-					if(!visited.contains(link) && visited.size() < 50){
-						visited.add(link);
-						queue.execute(new CrawlWorker(link));
+				try{
+					URI link = new URI(str).resolve(page);
+					synchronized(visited){
+						if(!visited.contains(link) && visited.size() < 50){
+							visited.add(link);
+							queue.execute(new CrawlWorker(link));
+						}
 					}
-				}
+				}catch(URISyntaxException e){}
 			}
 			data = HTMLCleaner.cleanHTML(data);
 			IndexInputBatch batch = new IndexInputBatch(page.toString());
@@ -69,6 +72,8 @@ public class WebCrawler {
 			}
 			index.addBatch(batch);
 			stringScanner.close();
+			synchronized(visited){
+				visited.notifyAll();
 			}
 		}
 	}
